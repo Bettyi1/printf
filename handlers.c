e6b1481fb2b90f297843a538091f8cf7d5adc4ce
Beatrice Irungu <bettyirungu15@gmail.com>
2023-04-22 06:04:30 -0700

    functions that print strings

---
+++ handlers.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * handle_print - prints argument as specified
+ * @fmt: string with format specifier
+ * @list: arguments.
+ * @ind: index iterator.
+ * @buffer: buffer
+ * @flags: flags
+ * @width: width.
+ * @precision: precision
+ * @size: size
+ * Return: 1, -1.
+ */
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int k = 0;
+	int len = 0;
+	int printed_char = -1
+
+	fmt_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	};
+	for (; fmt_types[k].fmt != '\0'; k++)
+		if (fmt[*ind] == fmt_types[k].fmt)
+			return (fmt_types[k].fn(list, buffer, flags, width, precision, size));
+
+	if (fmt_types[k].fmt == '\0')
+	{
+		if (fmt[*ind] == '\0')
+			return (-1);
+		len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		len += write(1, &fmt[*ind], 1);
+		return (len);
+	}
+	return (printed_char);
+}
