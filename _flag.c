e28d8fc45b2540e9433f919f66880fd1e357739d
Beatrice Irungu <bettyirungu15@gmail.com>
2023-04-24 09:06:33 -0700

    function prints flags

--- _flag.c
+++ _flag.c
@@ -3,23 +3,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+
 /**
- * get_flags - returns set flag
+ * f_flg - returns set flag
  * @format: pointer to format
  * @i: iterator value
  * Return: flags
  */
-int get_flags(const char *format, int *i)
+int f_flg(const char *format, int *i)
 {
 	int a;
 int b;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+	const int FLAGS_ARR[] = {FLAG_S, FLAG_A, FLAG_O, FLAG_SH, FLAG_P, 0};
 
 	for (b = *i + 1; format[b] != '\0'; b++)
 	{
-		for (a = 0 ; FLAGS_CH[a] != '\0'; a++)
+		for (a = 0; FLAGS_CH[a] != '\0'; a++)
 			if (format[b] == FLAGS_CH[a])
 			{
 				flags |= FLAGS_ARR[a];
