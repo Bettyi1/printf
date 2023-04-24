d1ecad8db34cbff3f07681e4cfca4a8fa75d5914
Beatrice Irungu <bettyirungu15@gmail.com>
2023-04-24 09:43:05 -0700

    multiple functions: unsigned nums, nums, and strings

--- multi_verse.c
+++ multi_verse.c
@@ -3,9 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
-
 /**
- * handle_write_char - function prints a string
+ * p_character - function prints a string
  * @buffer: buffer array
  * @flags:  checks active flags
  * @width: get width.
@@ -15,7 +14,7 @@
  *
  * Return: number of chars
  */
-int handle_write_char(char c, char buffer[],
+int p_character(char c, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int k = 0;
@@ -24,7 +23,7 @@ int handle_write_char(char c, char buffer[],
 	UNUSED(precision);
 	UNUSED(size);
 
-	if (flags & F_ZERO)
+	if (flags & FLAG_O)
 		space = '0';
 
 	buffer[k++] = c;
@@ -36,7 +35,7 @@ int handle_write_char(char c, char buffer[],
 		for (k = 0; k < width - 1; k++)
 			buffer[BUFF_SIZE - k - 2] = space;
 
-		if (flags & F_MINUS)
+		if (flags & FLAG_S)
 			return (write(1, &buffer[0], 1) +
 					write(1, &buffer[BUFF_SIZE - k - 1], width - 1));
 		else
@@ -48,7 +47,7 @@ int handle_write_char(char c, char buffer[],
 }
 
 /**
- * write_number - function prints a string
+ * p_wnumbr - function prints a string
  * @is_negative: arguments
  * @buffer: buffer array
  * @flags:  flags
@@ -59,7 +58,7 @@ int handle_write_char(char c, char buffer[],
  *
  * Return: char
  */
-int write_number(int is_negative, int ind, char buffer[],
+int p_wnumbr(int is_negative, int ind, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int length = BUFF_SIZE - ind - 1;
@@ -68,21 +67,21 @@ char zeros = 0;
 
 	UNUSED(size);
 
-	if ((flags & F_ZERO) && !(flags & F_MINUS))
+	if ((flags & FLAG_O) && !(flags & FLAG_S))
 		space = '0';
 	if (is_negative)
 		zeros = '-';
-	else if (flags & F_PLUS)
+	else if (flags & FLAG_A)
 		zeros = '+';
-	else if (flags & F_SPACE)
+	else if (flags & FLAG_P)
 		zeros = ' ';
 
-	return (write_num(ind, buffer, flags, width, precision,
+	return (p_num(ind, buffer, flags, width, precision,
 		length, space, zeros));
 }
 
 /**
- * write_num - function writes a number using buffer
+ * p_num - function writes a number using buffer
  * @buffer: buffer
  * @flags: flags
  * @ind: index
@@ -93,7 +92,7 @@ char zeros = 0;
  * @zeros: extra char
  * Return: Number of printed chars.
  */
-int write_num(int ind, char buffer[],
+int p_num(int ind, char buffer[],
 	int flags, int width, int prec,
 	int length, char space, char zeros)
 {
@@ -114,19 +113,19 @@ int write_num(int ind, char buffer[],
 		for (k = 1 ; k < width - length + 1 ; k++)
 			buffer[k] = space;
 		buffer[k] = '\0';
-		if (flags & F_MINUS && space == ' ')
+		if (flags & FLAG_S && space == ' ')
 		{
 			if (zeros)
 				buffer[--ind] = zeros;
 			return (write(1, &buffer[ind], length) + write(1, &buffer[1], k - 1));
 		}
-		else if (!(flags & F_MINUS) && space == ' ')
+		else if (!(flags & FLAG_S) && space == ' ')
 		{
 			if (zeros)
 				buffer[--ind] = zeros;
 			return (write(1, &buffer[1], k - 1) + write(1, &buffer[ind], length));
 		}
-		else if (!(flags & F_MINUS) && space == '0')
+		else if (!(flags & FLAG_S) && space == '0')
 		{
 			if (zeros)
 				buffer[--space_start] = zeros;
@@ -140,7 +139,7 @@ int write_num(int ind, char buffer[],
 }
 
 /**
- * write_unsgnd - function prints unsigned num.
+ * p_unsd - function prints unsigned num.
  * @is_negative: negative id
  * @ind: iterator
  * @buffer: buffer
@@ -151,7 +150,7 @@ int write_num(int ind, char buffer[],
  *
  * Return: unsigned num.
  */
-int write_unsgnd(int is_negative, int ind,
+int p_unsd(int is_negative, int ind,
 	char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -174,7 +173,7 @@ int len = BUFF_SIZE - ind - 1;
 		len++;
 	}
 
-	if ((flags & F_ZERO) && !(flags & F_MINUS))
+	if ((flags & FLAG_O) && !(flags & FLAG_S))
 		space = '0';
 
 	if (len < width)
@@ -184,7 +183,7 @@ int len = BUFF_SIZE - ind - 1;
 
 		buffer[j] = '\0';
 
-		if (flags & F_MINUS)
+		if (flags & FLAG_S)
 		{
 			return (write(1, &buffer[ind], len) + write(1, &buffer[0], j));
 		}
@@ -198,7 +197,7 @@ int len = BUFF_SIZE - ind - 1;
 }
 
 /**
- * write_pointer - prints a pointer
+ * p_wptr - prints a pointer
  * @buffer: buffer.
  * @ind: Iterator.
  * @length: length
@@ -210,7 +209,7 @@ int len = BUFF_SIZE - ind - 1;
  *
  * Return: pointer printed
  */
-int write_pointer(char buffer[], int ind, int length,
+int p_wptr(char buffer[], int ind, int length,
 	int width, int flags, char space, char zeros, int space_start)
 {
 	int a;
@@ -220,7 +219,7 @@ int write_pointer(char buffer[], int ind, int length,
 		for (a = 3; a < width - length + 3; a++)
 			buffer[a] = space;
 		buffer[a] = '\0';
-		if (flags & F_MINUS && space == ' ')
+		if (flags & FLAG_S && space == ' ')
 		{
 			buffer[--ind] = 'x';
 			buffer[--ind] = '0';
@@ -228,7 +227,7 @@ int write_pointer(char buffer[], int ind, int length,
 				buffer[--ind] = zeros;
 			return (write(1, &buffer[ind], length) + write(1, &buffer[3], a - 3));
 		}
-		else if (!(flags & F_MINUS) && space == ' ')
+		else if (!(flags & FLAG_S) && space == ' ')
 		{
 			buffer[--ind] = 'x';
 			buffer[--ind] = '0';
@@ -236,7 +235,7 @@ int write_pointer(char buffer[], int ind, int length,
 				buffer[--ind] = zeros;
 			return (write(1, &buffer[3], a - 3) + write(1, &buffer[ind], length));
 		}
-		else if (!(flags & F_MINUS) && space == '0')
+		else if (!(flags & FLAG_S) && space == '0')
 		{
 			if (zeros)
 				buffer[--space_start] = zeros;
