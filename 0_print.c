d6cfe8084cc23eb9d8b29ed0b686cb47a143b1db
Beatrice Irungu <bettyirungu15@gmail.com>
2023-04-25 14:15:27 -0700

    function produces format

--- 0_print.c
+++ 0_print.c
@@ -16,7 +16,7 @@ void write_content(char buffer[], int *index);
 int _printf(const char *format, ...)
 {
 
-	int k, output = 0, output_character = 0;
+	int k, output = 0, output_character = 0; /* two integer*/
 	int flags, width, precision, size, index = 0;
 
 	va_list argt;
